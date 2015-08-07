be28525c6d12161a5ffecfef6a2b35cb17cc355b
sadallo <sadallo@hotmail.com>
2015-08-07 14:27:24 -0400

    delete files

--- UMNewJobWebsite/Default.aspx.cs
+++ UMNewJobWebsite/Default.aspx.cs
@@ -4,18 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using MLApp;
-using UMNewJobWebsite.Business;
-using UMNewJobWebsite.Models;
-using UMNewJobWebsite.DomainDTO;
-using UMNewJobWebsite.Service.Plugin;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Service svc = new Service();
-        //List<JobDto> list = svc.selectJobNotDoneByRecruiteeId("40709433-B16C-4952-B662-08D28040C723");
-    
     }
 }
--- UMNewRecruiteeWebsite/Default.aspx.cs
+++ UMNewRecruiteeWebsite/Default.aspx.cs
@@ -4,12 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using MLApp;
-using UMNewRecruiteeWebsite.Business;
-using UMNewRecruiteeWebsite.Service.Interface;
-using UMNewRecruiteeWebsite.Service.Plugin;
-using UMNewRecruiteeWebsite.Models;
-using UMNewRecruiteeWebsite.DomainDTO;
 
 public partial class _Default : System.Web.UI.Page
 {
